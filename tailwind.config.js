b01b62a29874d2d32f7e280671d1066a95c82670
mame534424 <mame.534424@gmail.com>
2025-07-14 15:09:50 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        poppins:['Poppins']
+    },
+  },
+  },
+  plugins: [],
+}
+
